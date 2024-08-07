46436e7d1597d9872e28521102bf1ff72c075d0e
Kaik Xavier Marques de Oliveira <kaikmarques.ti@hotmail.com>
2024-08-07 08:45:24 -0300

    aula_cursoEmVideo

---
+++ aula6 -2 - dissecando uma variável.py
@@ -0,0 +1,11 @@
+a = input("digite algo: ")
+print("O tipo primitivo desse valor é ", type(a))
+print("Só tem espaços", a.isspace())
+print("É um número? ", a.isnumeric())
+print("É alfabético? ", a.isalpha())
+print("É alfanumérico? ", a.isalnum())
+print("Está em maiúsculas? ", a.isupper())
+print("Está em minúsculas?", a.islower())
+print("Está captalizada? ", a.istitle())
+
+Parei nesta aula  https://www.cursoemvideo.com/curso/python-3-mundo-1/aulas/tratando-dados-e-fazendo-contas/modulos/operadores-aritmeticos-2/
